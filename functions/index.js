79d5fa5ad4f6b7bba82e2aa0c39009b1bf669854
Tobias Wilker <twilker@phoenixcontact.com>
2022-08-07 07:31:44 +0200

    Add user management.

--- functions/index.js
+++ functions/index.js
@@ -12,6 +12,36 @@ exports.mirrorCron = functions.pubsub.schedule('every 5 minutes').onRun((context
     return null;
 });
 
+// On sign up.
+exports.processSignUp = functions.auth.user().onCreate(async (user) => {
+    var db = admin.database();
+    var whitelisted = await db.ref('users/whitelisted').get();
+
+    // Check if user meets role criteria.
+    if (user.email &&
+        user.emailVerified &&
+        whitelisted.val().includes(user.email)
+    ) {
+      const customClaims = {
+        whitelisted: true
+      };
+  
+      try {
+        // Set custom user claims on this newly created user.
+        await admin.auth().setCustomUserClaims(user.uid, customClaims);
+  
+        // Update real-time database to notify client to force refresh.
+        const metadataRef = db.ref('metadata/' + user.uid);
+  
+        // Set the refresh time to the current UTC timestamp.
+        // This will be captured on the client to force a token refresh.
+        await  metadataRef.set({refreshTime: new Date().getTime()});
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  });
+
 exports.mirrorTest = functions.runWith({ secrets: ["API_ID", "API_SECRET"] }).https.onRequest(async (req, res) => {
     let body = '';
 
